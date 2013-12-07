e8e64409e3ea8cf1d521e2ff962aa10585d4262f
ryan <odonnellryanc@gmail.com>
2013-12-07 16:12:24 -0500

    added pages

--- forms/user_forms.py
+++ forms/user_forms.py
@@ -1,5 +1,4 @@
-from wtforms import Form, PasswordField, TextField, validators
+from wtforms import Form, TextField, validators
 
 class register(Form):
-    Email = TextField(u'Username', [validators.Length(min=1)])
-    Password = PasswordField(u'Password', [validators.Length(min=1)])
+    Email = TextField(u'Email', [validators.Length(min=1)])
